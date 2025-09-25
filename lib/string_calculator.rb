f42323d5bda7fbcda110457625384bf2558186b3
Abha Verma <abhaverma53@gmail.com>
2025-09-25 21:11:51 +0530

    [single number test added]

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,7 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers.strip.empty?
+    tokens = numbers.split(/,|\n/)
+    return tokens.first.to_i if tokens.size == 1
   end
 end
