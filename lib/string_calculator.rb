e79029b947ccd7c1d80419a50b0d6805be2d5615
Abha Verma <abhaverma53@gmail.com>
2025-09-25 21:43:43 +0530

    [custom delimeter added]

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,17 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers.strip.empty?
 
-    tokens = numbers.split(/,|\n/).reject(&:empty?)
+    str = numbers.dup
+    delimiter_regex = /,|\n/
+
+    if str.start_with?("//")
+      header, rest = str.split("\n", 2)
+      custom = header[2..-1] || ""
+      delimiter_regex = Regexp.new(Regexp.escape(custom))
+      str = rest || ""
+    end
+
+    tokens = str.split(delimiter_regex).reject(&:empty?)
     ints = tokens.map(&:to_i)
     ints.reduce(0, :+)
   end
